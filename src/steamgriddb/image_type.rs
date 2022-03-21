b4dbcdd29c7f07e0ef435cddd8e2017dda0d9e10
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-03-21 21:34:27 +0100

    Download official steam images as backup (#53)

--- src/steamgriddb/image_type.rs
+++ src/steamgriddb/image_type.rs
@@ -17,4 +17,15 @@ impl ImageType {
             ImageType::BigPicture => format!("{}_bigpicture.png", app_id),
         }
     }
+
+    pub fn steam_url<S:AsRef<str>>(&self,steam_app_id:S,mtime:u64) -> String{
+        let steam_app_id = steam_app_id.as_ref();
+        match self{
+            ImageType::Hero => format!("https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_hero.jpg?t={}",steam_app_id,mtime),
+            ImageType::Grid => format!("https://cdn.cloudflare.steamstatic.com/steam/apps/{}/library_600x900_2x.jpg?t={}",steam_app_id,mtime),
+            ImageType::WideGrid => format!("https://cdn.cloudflare.steamstatic.com/steam/apps/{}/header.jpg?t={}",steam_app_id,mtime),
+            ImageType::Logo => format!("https://cdn.cloudflare.steamstatic.com/steam/apps/{}/logo.png?t={}",steam_app_id,mtime),
+            ImageType::BigPicture => format!("https://cdn.cloudflare.steamstatic.com/steam/apps/{}/header.jpg?t={}",steam_app_id,mtime),
+        }
+    }
 }
