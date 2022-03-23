6a849b49e036c7c63a95611b3b516f749b0af2cc
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-03-23 23:11:15 +0100

    Write collections

--- src/steam/collections.rs
+++ src/steam/collections.rs
@@ -6,11 +6,29 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use rusty_leveldb::{DBIterator, LdbIterator, Options, DB};
 
+const BOILR_TAG: &'static str = "boilr";
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct SteamCollection {
+#[serde(untagged)]
+enum SteamCollection {
+    Actual(ActualSteamCollection),
+    Deleted(DeletedCollection),
+}
+
+impl SteamCollection {
+    pub fn is_boilr_collection(&self) -> bool {
+        match self {
+            SteamCollection::Actual(actual) => actual.is_boilr_collection(),
+            SteamCollection::Deleted(_) => false,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ActualSteamCollection {
     key: String,
     timestamp: u64,
-    value: String,
+    value: String, //For custom collections, this value is a ValueCollection json serialized
     #[serde(
         skip_serializing_if = "Option::is_none",
         rename = "conflictResolutionMethod"
@@ -22,7 +40,7 @@ pub struct SteamCollection {
     version: Option<String>,
 }
 
-impl SteamCollection {
+impl ActualSteamCollection {
     fn new<B: AsRef<str>>(name: B, ids: &Vec<usize>) -> Self {
         let name = name.as_ref();
         let key = format!("user-collections.{}", name_to_key(name));
@@ -33,7 +51,7 @@ impl SteamCollection {
             .expect("Time went backwards");
         let timestamp = since_the_epoch.as_secs();
 
-        SteamCollection {
+        ActualSteamCollection {
             key,
             timestamp,
             value,
@@ -42,73 +60,137 @@ impl SteamCollection {
             version: None,
         }
     }
+
+    pub fn is_boilr_collection(&self) -> bool {
+        self.key
+            .starts_with(&format!("user-collections.{}", BOILR_TAG))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ValueCollection {
+    id: String,
+    name: String,
+    added: Vec<usize>,
+    removed: Vec<usize>,
+}
+
+impl ValueCollection {
+    fn new<S: AsRef<str>>(name: S, game_ids: &Vec<usize>) -> Self {
+        let name = name.as_ref();
+        let id = name_to_key(name);
+        let value = ValueCollection {
+            id,
+            name: name.to_string(),
+            added: game_ids.clone(),
+            removed: vec![],
+        };
+        value
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 
-pub struct DeletedCollection {
+struct DeletedCollection {
     key: String,
     timestamp: usize,
     is_deleted: bool,
     version: String,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(untagged)]
-pub enum SteamCollectionType {
-    Actual(SteamCollection),
-    Deleted(DeletedCollection),
+pub struct Collection {
+    name: String,
+    game_ids: Vec<usize>,
 }
 
-fn get_categories_data<S: AsRef<str>>(
+pub fn write_collections<S: AsRef<str>>(
+    steam_user_id: S,
+    collections: &Vec<Collection>,
+) -> Result<(), Box<dyn Error>> {
+    let steam_user_id = steam_user_id.as_ref();
+    let new_collections: Vec<(String, SteamCollection)> = collections
+        .iter()
+        .map(|c| {
+            let collection = ActualSteamCollection::new(&c.name, &c.game_ids);
+            (collection.key.clone(), SteamCollection::Actual(collection))
+        })
+        .collect();
+
+    let mut db = open_db()?;
+
+    let current_categories = get_categories(steam_user_id, &mut db)?;
+    //this is a collection of collections, known as a category
+    for (category_key, mut collections) in current_categories {
+        collections.retain(|(_key, collection)| !collection.is_boilr_collection());
+        collections.extend(new_collections.clone());
+        save_category(category_key, collections, &mut db)?
+    }
+
+    
+
+    Ok(())
+}
+
+fn save_category<S: AsRef<str>>(
+    category_key: S,
+    category: Vec<(String, SteamCollection)>,
+    db: &mut DB,
+) -> Result<(), Box<dyn Error>> {
+    let json = serde_json::to_string(&category)?;
+    let prefixed_json = format!("\u{1}{}", json);
+    db.put(category_key.as_ref().as_bytes(),prefixed_json.as_bytes())?;
+    db.flush()?;
+    Ok(())
+}
+
+fn get_categories<S: AsRef<str>>(
     steamid: S,
-) -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let steamid = steamid.as_ref();
-
-    let keyprefix = format!(
-        "_https://steamloopback.host\u{0000}\u{0001}U{}-cloud-storage-namespace",
-        steamid
-    );
-
-    let location = get_level_db_location();
-    if let None = location {
-        todo!();
-    };
-
-    let mut db = DB::open(location.unwrap(), Options::default())?;
-    let namespace_keys = get_namespace_keys(&keyprefix, &mut db);
-
+    db: &mut DB,
+) -> Result<HashMap<String, Vec<(String, SteamCollection)>>, Box<dyn Error>> {
+    let namespace_keys = get_namespace_keys(steamid, db);
+    dbg!(&namespace_keys);
     let mut db_iter = db.new_iter()?;
     let mut res = HashMap::new();
-    while let Some(item) = db_iter.next() {
-        let key = String::from_utf8_lossy(&item.0).to_string();
+    while let Some((key_bytes, data_bytes)) = db_iter.next() {
+        let key = String::from_utf8_lossy(&key_bytes).to_string();
+        //make sure that what we are looking at is a collection
+        //there are other things in this db as well
         if namespace_keys.contains(&key) {
-            let prefix = format!("{}-", &keyprefix);
-            let id = key.replace(prefix.as_str(), "");
-            let data = String::from_utf8_lossy(&item.1).to_string();
-            res.insert(id, data);
+            let data = String::from_utf8_lossy(&data_bytes).to_string();
+            let collections = parse_steam_collections(&data)?;
+            res.insert(key, collections);
         }
     }
     Ok(res)
 }
 
-fn get_namespace_keys<S: AsRef<str>>(keyprefix: &S, db: &mut DB) -> HashSet<String> {
-    let namespaces_key = format!("{}s", keyprefix.as_ref());
+fn open_db() -> Result<DB, Box<dyn Error>> {
+    let location = get_level_db_location();
+    if let None = location {
+        todo!()
+    };
+    Ok(DB::open(location.unwrap(), Options::default())?)
+}
+
+fn get_namespace_keys<S: AsRef<str>>(steamid: S, db: &mut DB) -> HashSet<String> {
+    let keyprefix = get_steam_user_prefix(steamid);
+
+    let namespaces_key = format!("{}s", keyprefix);
     let key_bytes = namespaces_key.as_bytes();
-    let collections = get_collections(db, key_bytes).unwrap_or_default();
-    let namespace_keys = collections
+    let namespaces = get_namespaces(db, key_bytes).unwrap_or_default();
+    let namespace_keys = namespaces
         .iter()
-        .map(|c| format!("{}-{}", keyprefix.as_ref(), c.0))
+        .map(|c| format!("{}-{}", keyprefix, c.0))
         .collect();
     namespace_keys
 }
 
-fn get_collections(db: &mut DB, key_bytes: &[u8]) -> Option<Vec<(i32, String)>> {
+fn get_namespaces(db: &mut DB, key_bytes: &[u8]) -> Option<Vec<(i32, String)>> {
     match db.get(key_bytes) {
         Some(got) => {
             let collection_bytes = got.as_slice();
             let collectin_str = String::from_utf8_lossy(&collection_bytes)[1..].to_string();
-            let collection = parse_collections(collectin_str);
+            let collection = serde_json::from_str(&collectin_str).unwrap_or_default();
             Some(collection)
         }
         _ => None,
@@ -121,7 +203,8 @@ fn get_level_db_location() -> Option<PathBuf> {
         Ok(home) => {
             let path = Path::new(&home)
                 .join(".steam")
-                .join("Steam")
+                .join("steam")
+                .join("config")
                 .join("htmlcache")
                 .join("Local Storage")
                 .join("leveldb");
@@ -152,27 +235,9 @@ fn get_level_db_location() -> Option<PathBuf> {
     }
 }
 
-const BOILR_TAG: &'static str = "boilr";
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct ValueCollection {
-    id: String,
-    name: String,
-    added: Vec<usize>,
-    removed: Vec<usize>,
-}
-
 fn serialize_collection_value<S: AsRef<str>>(name: S, game_ids: &Vec<usize>) -> String {
-    let name = name.as_ref();
-    let key = name_to_key(name);
-    let value = ValueCollection {
-        id: key,
-        name: name.to_string(),
-        added: game_ids.clone(),
-        removed: vec![],
-    };
+    let value = ValueCollection::new(name, game_ids);
     let value_json = serde_json::to_string(&value).expect("Should be able to serialize known type");
-
     value_json
 }
 
@@ -183,23 +248,35 @@ fn name_to_key<S: AsRef<str>>(name: S) -> String {
     key
 }
 
-fn parse_collections<S: AsRef<str>>(input: S) -> Vec<(i32, String)> {
-    serde_json::from_str(input.as_ref()).unwrap_or_default()
-}
-
-fn parse_steam_collections<S: AsRef<str>>(input: S) -> Vec<(String, SteamCollectionType)> {
+fn parse_steam_collections<S: AsRef<str>>(
+    input: S,
+) -> Result<Vec<(String, SteamCollection)>, Box<dyn Error>> {
     let input = input.as_ref();
     let input = if input.starts_with("\u{1}") {
         input[1..].to_string()
     } else {
         input.to_string()
     };
-    serde_json::from_str(&input).unwrap()
+    let res = serde_json::from_str::<Vec<(String, SteamCollection)>>(&input)?;
+    Ok(res)
 }
 
-fn serialize_steam_collections(input: Vec<(String, SteamCollectionType)>) -> String {
+fn serialize_steam_collections(input: Vec<(String, SteamCollection)>) -> String {
     let res = serde_json::to_string(&input).unwrap();
     format!("\u{1}{}", res)
+}
+
+fn local_key_to_global<A: AsRef<str>, B: AsRef<str>>(steamid: A, local_key: B) -> String {
+    let user_prefix = get_steam_user_prefix(steamid);
+    format!("{}-{}", user_prefix, local_key.as_ref())
+}
+
+fn get_steam_user_prefix<S: AsRef<str>>(steamid: S) -> String {
+    let keyprefix = format!(
+        "_https://steamloopback.host\u{0000}\u{0001}U{}-cloud-storage-namespace",
+        steamid.as_ref()
+    );
+    keyprefix
 }
 
 #[cfg(test)]
@@ -207,14 +284,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn can_serialize_collection(){
+
+    fn write_collections_test() {
+        let steamid = "10342635";
+        let colllections = vec![Collection {
+            name: "This is how we do".to_string(),
+            game_ids: vec![265930, 751780, 433340, 361420, 337340, 1055540],
+        }];
+        write_collections(
+            steamid,
+            &colllections,
+        ).unwrap();
+    }
+
+    #[test]
+    fn can_serialize_collection() {
         let games = vec![312200];
-        let mut collection = SteamCollection::new("Itch",&games);
+        let mut collection = ActualSteamCollection::new("Itch", &games);
         collection.timestamp = 1647763452;
         let json = serde_json::to_string(&collection).unwrap();
         let expected = include_str!("../testdata/leveldb/test_collection.json");
         assert_eq!(json, expected);
-
     }
 
     #[test]
@@ -228,7 +318,7 @@ mod tests {
     #[test]
     fn can_serialize_and_deserialize() {
         let input = include_str!("../testdata/leveldb/testcollections.json");
-        let parsed = parse_steam_collections(input);
+        let parsed = parse_steam_collections(input).unwrap();
         let serialized = serialize_steam_collections(parsed);
         assert_eq!(input, &serialized);
     }
@@ -236,33 +326,7 @@ mod tests {
     #[test]
     fn can_parse_categories() {
         let input = include_str!("../testdata/leveldb/testcollections.json");
-        let collection = parse_steam_collections(input);
+        let collection = parse_steam_collections(input).unwrap();
         assert_eq!(28, collection.len())
-    }
-
-    #[test]
-    fn can_parse_collections_empty() {
-        let res = parse_collections("");
-        let expected = Vec::<(i32, String)>::new();
-        assert_eq!(expected, res);
-    }
-
-    #[test]
-    fn can_parse_collections_empty_list() {
-        let res = parse_collections("[]");
-        let expected = Vec::<(i32, String)>::new();
-        assert_eq!(expected, res);
-    }
-
-    #[test]
-    fn can_parse_collections_single() {
-        let res = parse_collections("[[1,\"772\"]]");
-        assert_eq!(vec![(1, "772".to_string())], res);
-    }
-
-    #[test]
-    fn can_parse_collections_multiple() {
-        let res = parse_collections("[[1,\"772\"],[2,\"773\"]]");
-        assert_eq!(vec![(1, "772".to_string()), (2, "773".to_string())], res);
     }
 }
