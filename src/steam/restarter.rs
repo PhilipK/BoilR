af43ad6ac7f1ea33ed81f44310eb62ca8d3d3f5c
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-04-15 08:53:05 +0200

    Add linux support to steam restarter

--- src/steam/restarter.rs
+++ src/steam/restarter.rs
@@ -12,20 +12,19 @@ pub fn ensure_steam_stopped() {
     let processes = s.processes_by_name(&steam_name);
     for process in processes {
         let mut s = System::new();
-        process.kill();
+        process.kill_with(sysinfo::Signal::Quit);
+        process.kill_with(sysinfo::Signal::Kill);
         while s.refresh_process(process.pid()) {
             println!("Waiting for steam to stop");
             sleep(Duration::from_millis(500));
+            process.kill_with(sysinfo::Signal::Quit);
+            process.kill_with(sysinfo::Signal::Kill);
         }
     }
 }
-
+#[cfg(target_os = "windows")]
 pub fn ensure_steam_started(settings: &super::SteamSettings) {
-    #[cfg(target_os = "windows")]
     let steam_name = "steam.exe";
-    #[cfg(target_os = "linux")]
-    let steam_name = "steam";
-
     let s = System::new_all();
     let mut processes = s.processes_by_name(&steam_name);
     if processes.next().is_none() {
@@ -39,5 +38,20 @@ pub fn ensure_steam_started(settings: &super::SteamSettings) {
                 println!("Failed to start steam: {:?}", e);
             };
         }
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn ensure_steam_started(_settings: &super::SteamSettings) {
+    let steam_name = "steam";
+    let s = System::new_all();
+    let mut processes = s.processes_by_name(&steam_name);
+    if processes.next().is_none() {
+        //no steam, we need to start it
+        println!("Starting steam");
+        let mut command = Command::new(&steam_name);
+        if let Err(e) = command.spawn() {
+            println!("Failed to start steam: {:?}", e);
+        };
     }
 }
