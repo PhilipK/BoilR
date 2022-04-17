49a71c203bb833c18fdd043feaefb12092f4278a
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-04-17 21:32:32 +0200

    Add icon to exe in windows

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::io;
+#[cfg(windows)] use winres::WindowsResource;
+
+fn main() -> io::Result<()> {
+    #[cfg(windows)] {
+        WindowsResource::new()
+            .set_icon("resources/logo.ico")
+            .compile()?;
+    }
+    Ok(())
+}
