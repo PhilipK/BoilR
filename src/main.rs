4f7af6610267f16f9ffcff415fba5dd0caedcb07
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-05-01 07:08:44 +0200

    move migration to seperate file

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod heroic;
 mod itch;
 mod legendary;
 mod lutris;
+mod migration;
 mod origin;
 mod platform;
 mod settings;
@@ -14,11 +15,11 @@ mod steamgriddb;
 mod sync;
 mod ui;
 mod uplay;
-use std::{error::Error, path::Path};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     ensure_config_folder();
-    migrate_config();
+    migration::migrate_config();
 
     let mut args = std::env::args();
     if args.len() > 1 && args.nth(1).unwrap_or_default() == "--no-ui" {
@@ -32,44 +33,4 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn ensure_config_folder() {
     let path = config::get_config_folder();
     let _ = std::fs::create_dir_all(&path);
-}
-
-fn migrate_config() {
-    let version = &settings::Settings::new()
-        .map(|s| s.config_version)
-        .unwrap_or_default();
-
-    let mut save_version = false;
-    if version.is_none() {
-        //Migration from 0 to 1
-        let old_path = &Path::new("config.toml");
-        if old_path.exists() {
-            println!("Migrating from configuration version 0 to version 1");
-            let new_path = config::get_config_file();
-            println!("Your configuration file will be moved to {:?}", new_path);
-            let _ = std::fs::copy(old_path, new_path);
-            let _ = std::fs::remove_file(old_path);
-        }
-
-        let old_path = &Path::new(".thumbnails");
-        if old_path.exists() {
-            //thumbnails are just cache can be removed
-            let _ = std::fs::remove_dir_all(old_path);
-        }
-
-        let old_path = &Path::new("cache.json");
-        if old_path.exists() {
-            let new_path = config::get_cache_file();
-            let _ = std::fs::copy(old_path, new_path);
-            let _ = std::fs::remove_file(old_path);
-        }
-        save_version = true;
-    }
-
-    if save_version {
-        if let Ok(mut settings) = settings::Settings::new() {
-            settings.config_version = Some(1);
-            ui::MyEguiApp::save_settings_to_file(&settings);
-        }
-    }
 }
