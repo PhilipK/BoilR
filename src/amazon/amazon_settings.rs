138020daad7d396545c11bbb9d94c4d02262eef5
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-05-22 09:03:22 +0200

    Launch through exe for amazon games

--- src/amazon/amazon_settings.rs
+++ src/amazon/amazon_settings.rs
@@ -3,4 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct AmazonSettings {
     pub enabled: bool,
+    pub launcher_location: Option<String>
 }
