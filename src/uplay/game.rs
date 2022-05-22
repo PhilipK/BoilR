9dadc4035c6a6583c58151cacbf8eeeb4c979763
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-05-22 13:56:21 +0200

    Launch Uplay games through exe

--- src/uplay/game.rs
+++ src/uplay/game.rs
@@ -1,3 +1,5 @@
+use std::path::{PathBuf, Path};
+
 use steam_shortcuts_util::shortcut::{Shortcut, ShortcutOwned};
 
 #[derive(Clone)]
@@ -5,11 +7,14 @@ pub(crate) struct Game {
     pub(crate) name: String,
     pub(crate) icon: String,
     pub(crate) id: String,
+    pub(crate) launcher: PathBuf
 }
 
 impl From<Game> for ShortcutOwned {
     fn from(game: Game) -> Self {
-        let launch = format!("uplay://launch/{}", game.id);
-        Shortcut::new("0", &game.name, &launch, "", &game.icon, "", "").to_owned()
+        let launch = format!("\"uplay://launch/{}/0\"", game.id);
+        let start_dir = game.launcher.parent().unwrap_or(Path::new("")).to_string_lossy();
+        let exe = format!("\"{}\"",game.launcher.to_string_lossy());
+        Shortcut::new("0", &game.name, &exe, &start_dir, &game.icon, "", &launch).to_owned()
     }
 }
