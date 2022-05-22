cb53571c78bde32770f03f22908698f25604e8ae
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-05-22 21:22:17 +0200

    Add option to launch heroic games through launcher

--- src/heroic/settings.rs
+++ src/heroic/settings.rs
@@ -3,4 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct HeroicSettings {
     pub enabled: bool,
+    pub launch_games_through_heroic: bool,
 }
