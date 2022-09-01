c6e7f3ce9413628c2fd4e3b307a89638481ff141
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-09-01 14:33:35 +0200

    Guess at image format instead of extension

--- src/ui/defines.rs
+++ src/ui/defines.rs
@@ -44,7 +44,7 @@ pub mod ui_images {
     }
 
    pub fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
-        let image = image::io::Reader::open(path)?.decode()?;
+        let image = image::io::Reader::open(path)?.with_guessed_format()?.decode()?;
         let size = [image.width() as _, image.height() as _];
         let image_buffer = image.to_rgba8();
         let pixels = image_buffer.as_flat_samples();
