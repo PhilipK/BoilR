c933c84b0cffd0f5a2d9bbfe799c16e9ad1d841f
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-09-15 10:04:17 +0200

    Always download newest cargo-generator py

--- flatpak/update-cargo-lock-json.sh
+++ flatpak/update-cargo-lock-json.sh
@@ -1,2 +1,4 @@
+curl https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/cargo/flatpak-cargo-generator.py --output flatpak-cargo-generator.py
 cd ..
 python3 ./flatpak/flatpak-cargo-generator.py ./Cargo.lock -o ./flatpak/cargo-lock.json
+rm ./flatpak/flatpak-cargo-generator.py
