a48ce7f1f45b1e7196408bcdd277aae0e4945ff6
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-10-11 18:00:57 +0200

    Customize the games_folder in minigalaxy

--- src/platforms/minigalaxy/platform.rs
+++ src/platforms/minigalaxy/platform.rs
@@ -11,6 +11,7 @@ pub struct MiniGalaxyPlatform {
 struct Settings {
     enabled: bool,
     create_symlinks: bool,
+    games_folder: Option<String>,
 }
 
 impl Default for Settings{
@@ -19,7 +20,7 @@ impl Default for Settings{
         let enabled = true;
         #[cfg(target_family = "windows")]
         let enabled = false;
-        Self { enabled, create_symlinks:false }
+        Self { enabled, create_symlinks:false, games_folder:None }
     }
 }
 
@@ -70,8 +71,13 @@ impl GamesPlatform for MiniGalaxyPlatform{
     }
 
     fn get_shortcut_info(&self) -> eyre::Result<Vec<crate::platforms::ShortcutToImport>> {
-        let home = std::env::var("HOME")?;
-        let games_folder = std::path::Path::new(&home).join("GOG Games");
+
+        let games_folder = match &self.settings.games_folder{
+            Some(custom_folder) => std::path::Path::new(&custom_folder).to_path_buf(),
+            None => {
+                get_default_folder_path()?
+            },
+        };
         let dirs = games_folder.read_dir()?;
         let mut game_folders = vec![];
         for game_folder in dirs{
@@ -90,10 +96,27 @@ impl GamesPlatform for MiniGalaxyPlatform{
     fn render_ui(&mut self, ui: &mut egui::Ui) {
         ui.heading("Mini Galaxy");
         ui.checkbox(&mut self.settings.enabled, "Import from Mini Galaxy");
+        let mut path_text = self.settings.games_folder.clone().unwrap_or_else(get_default_folder_string);
+        if ui.text_edit_singleline(&mut path_text).changed(){
+            if path_text.trim().is_empty(){
+                self.settings.games_folder = None;
+            }else{
+                self.settings.games_folder = Some(path_text);
+            }
+        }
+
         #[cfg(target_family = "unix")]
         if self.settings.enabled {
             ui.checkbox(&mut self.settings.create_symlinks, "Create symlinks");
-
         }
     }
+}
+
+fn get_default_folder_string() -> String {
+    get_default_folder_path().unwrap_or_default().to_string_lossy().to_string()
+}
+
+fn get_default_folder_path() -> eyre::Result<std::path::PathBuf> {
+    let home = std::env::var("HOME")?;
+    Ok(std::path::Path::new(&home).join("GOG Games"))
 }
