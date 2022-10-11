674753aaec459499b45912e1bc1f2d2c345ccc8e
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-10-11 17:43:11 +0200

    Impelment mini galaxy platform

--- src/platforms/minigalaxy/platform.rs
+++ src/platforms/minigalaxy/platform.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::platforms::{GamesPlatform, FromSettingsString, load_settings};
+use crate::platforms::{GamesPlatform, FromSettingsString, load_settings, GogShortcut, NeedsPorton};
 
 #[derive(Clone)]
 pub struct MiniGalaxyPlatform {
@@ -10,6 +10,7 @@ pub struct MiniGalaxyPlatform {
 #[derive(Deserialize, Serialize, Clone)]
 struct Settings {
     enabled: bool,
+    create_symlinks: bool,
 }
 
 impl Default for Settings{
@@ -18,10 +19,34 @@ impl Default for Settings{
         let enabled = true;
         #[cfg(target_family = "windows")]
         let enabled = false;
-        Self { enabled }
+        Self { enabled, create_symlinks:false }
     }
 }
 
+
+impl NeedsPorton<MiniGalaxyPlatform> for GogShortcut{
+    #[cfg(target_family = "unix")]
+    fn needs_proton(&self, _platform: &MiniGalaxyPlatform) -> bool {
+        //TODO check if we can do better than just always true
+        //this might be a linux game
+        true
+    }
+
+    #[cfg(not(target_family = "unix"))]
+    fn needs_proton(&self, _platform: &MiniGalaxyPlatform) -> bool {
+        false
+    }
+
+    #[cfg(not(target_family = "unix"))]
+    fn create_symlinks(&self, _platform: &MiniGalaxyPlatform) -> bool {
+        false
+    }
+
+    #[cfg(target_family = "unix")]
+    fn create_symlinks(&self, platform: &MiniGalaxyPlatform) -> bool {
+        platform.settings.create_symlinks
+    }
+}
 
 impl FromSettingsString for MiniGalaxyPlatform {
     fn from_settings_string<S: AsRef<str>>(s: S) -> Self {
@@ -45,7 +70,17 @@ impl GamesPlatform for MiniGalaxyPlatform{
     }
 
     fn get_shortcut_info(&self) -> eyre::Result<Vec<crate::platforms::ShortcutToImport>> {
-        todo!()
+        let home = std::env::var("HOME")?;
+        let games_folder = std::path::Path::new(&home).join("GOG Games");
+        let dirs = games_folder.read_dir()?;
+        let mut game_folders = vec![];
+        for game_folder in dirs{
+            if let Ok(game_folder) = game_folder{
+                game_folders.push(game_folder.path().to_owned());
+            }
+        }
+        let gog_games = crate::platforms::get_gog_shortcuts_from_game_folders(game_folders);
+        crate::platforms::to_shortcuts(self, Ok(gog_games))
     }
 
     fn get_settings_serilizable(&self) -> String {
@@ -55,5 +90,10 @@ impl GamesPlatform for MiniGalaxyPlatform{
     fn render_ui(&mut self, ui: &mut egui::Ui) {
         ui.heading("Mini Galaxy");
         ui.checkbox(&mut self.settings.enabled, "Import from Mini Galaxy");
+        #[cfg(target_family = "unix")]
+        if self.settings.enabled {
+            ui.checkbox(&mut self.settings.create_symlinks, "Create symlinks");
+
+        }
     }
 }
