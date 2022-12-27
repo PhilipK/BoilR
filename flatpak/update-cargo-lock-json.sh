7aa9cc55bd4c2be58337326595e882e79ddc00a3
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-12-27 15:45:04 +0100

    Add pip install toml to flatpak cargo lock

--- flatpak/update-cargo-lock-json.sh
+++ flatpak/update-cargo-lock-json.sh
@@ -1,4 +1,5 @@
 curl https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/cargo/flatpak-cargo-generator.py --output flatpak-cargo-generator.py
 cd ..
+pip install toml
 python3 ./flatpak/flatpak-cargo-generator.py ./Cargo.lock -o ./flatpak/cargo-lock.json
 rm ./flatpak/flatpak-cargo-generator.py
