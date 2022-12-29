999499f0d867ed02c9976fd3ba7a0d524c03dbaa
Philip Kristoffersen <philipkristoffersen@gmail.com>
2022-12-29 16:19:20 +0100

    Remove unused imports

--- src/ui/images/ui_image_download.rs
+++ src/ui/images/ui_image_download.rs
@@ -1,7 +1,6 @@
 use super::{
     constants::POSSIBLE_EXTENSIONS,
     gamemode::GameMode,
-    gametype::GameType,
     hasimagekey::HasImageKey,
     image_select_state::ImageSelectState,
     pages::{
@@ -19,7 +18,7 @@ use std::path::Path;
     config::get_thumbnails_folder,
     steam::get_shortcuts_paths,
     steam::{get_installed_games, SteamUsersInfo},
-    steamgriddb::{get_image_extension, get_query_type, CachedSearch, ImageType, ToDownload},
+    steamgriddb::{get_image_extension, get_query_type,  ImageType, ToDownload},
     sync::{download_images, SyncProgress},
     ui::{ui_images::load_image_from_path, FetcStatus, MyEguiApp},
 };
